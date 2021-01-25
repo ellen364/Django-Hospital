e1ed5233d13f32f0cb658bc6a11de6ada391a3c1
Ellen <38250543+ellen364@users.noreply.github.com>
2021-01-25 08:31:14 +0000

    Update paths to use new config directory
    Files like settings.py are now in config/ not DjangoHospital/

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "DjangoHospital.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "DjangoHospital.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
